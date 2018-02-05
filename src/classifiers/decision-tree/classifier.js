22b0a80123097ad2c34b97480d43c5daaca0f46b
Jesper van Engelen <contact@jepps.nl>
2018-02-05 20:04:01 +0100

    Add maximum depth controls to decision tree

--- src/classifiers/decision-tree/classifier.js
+++ src/classifiers/decision-tree/classifier.js
@@ -4,4 +4,5 @@ import jsmlt from '@jsmlt/jsmlt';
 export default controls => (new jsmlt.Supervised.Trees.DecisionTree({
   criterion: controls.splittingCriterion,
   numFeatures: 1.0,
+  maxDepth: controls.maxDepthEnabled ? controls.maxDepth : -1,
 }));
--- src/classifiers/decision-tree/controls.js
+++ src/classifiers/decision-tree/controls.js
@@ -1,14 +1,75 @@
 // Standard imports
 import { Component } from 'react';
+import Slider from 'rc-slider';
+
+// Styles
+import 'rc-slider/assets/index.css';
+
+// Element for slider with tooltip
+const SliderWithTooltip = Slider.createSliderWithTooltip(Slider);
 
 // Default controls
 export const getDefaultControls = () => ({
   splittingCriterion: 'gini',
+  maxDepthEnabled: false,
+  maxDepth: 2,
 });
+
+const ControlsMaxDepth = ({ maxDepth, onChange }) => {
+  const marks = {
+    1: 1,
+    2: 2,
+    5: 5,
+    10: 10,
+    20: 20,
+  };
+
+  return (
+    <fieldset>
+      <label>Maximum depth</label>
+      <div className="slider">
+        <SliderWithTooltip
+          included={true}
+          marks={marks}
+          value={maxDepth}
+          min={1}
+          max={20}
+          onChange={onChange}
+        />
+      </div>
+    </fieldset>
+  );
+};
+
+export const ControlsMaxDepthAll = ({ maxDepthEnabled, maxDepth, onChangeMaxDepthEnabled,
+  onChangeMaxDepth}) => (
+  <fieldset>
+    <label>
+      <input
+        onChange={e => onChangeMaxDepthEnabled(e.target.checked)}
+        type="checkbox"
+        checked={maxDepthEnabled}
+      />
+      Enable maximum depth
+    </label>
+    {maxDepthEnabled && <ControlsMaxDepth
+      maxDepth={maxDepth}
+      onChange={x => onChangeMaxDepth(x)}
+    />}
+  </fieldset>
+);
 
 export default class Controls extends Component {
   handleChangeSplittingCriterion(splittingCriterion) {
     this.props.onChange({ splittingCriterion });
+  }
+
+  handleChangeMaxDepth(maxDepth) {
+    this.props.onChange({ maxDepth });
+  }
+
+  handleChangeMaxDepthEnabled(maxDepthEnabled) {
+    this.props.onChange({ maxDepthEnabled });
   }
 
   render() {
@@ -24,6 +85,12 @@ export default class Controls extends Component {
             <option value="entropy">Information gain</option>
           </select>
         </fieldset>
+        <ControlsMaxDepthAll
+          maxDepthEnabled={this.props.maxDepthEnabled}
+          maxDepth={this.props.maxDepth}
+          onChangeMaxDepthEnabled={x => this.handleChangeMaxDepthEnabled(x)}
+          onChangeMaxDepth={x => this.handleChangeMaxDepth(x)}
+        />
       </div>
     );
   }
